1412d24a522a1a25d1405a2b06163256a7d2003c
PalakHDesai <palakdesai72525@gmail.com>
2015-09-28 12:11:34 +0530

    Nested views

---
+++ Js/Main.js
@@ -0,0 +1,25 @@
+﻿var app = angular.module("MyApp", ["ui.router"]);
+//app.controller('MyController', function ($scope) {
+
+//});
+
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+
+    $stateProvider
+        .state("View1", {
+            url: "/View1",
+            templateUrl: "View1.html"
+        })
+        .state("View2", {
+            url: "/View2",
+            templateUrl: "View2.html"
+        })
+        .state("View3", {
+            url: "/View3",
+            templateUrl: "View3.html"
+        });
+
+
+});
